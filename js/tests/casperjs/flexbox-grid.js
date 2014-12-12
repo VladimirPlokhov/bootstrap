7cf2f811487f7531af156f68bdc9f2bfffc95b13
Vladimir Plokhov <vladimirplokhov@gmail.com>
2014-12-12 16:57:43 +0100

    Update casperjs script (add viewports)

--- js/tests/casperjs/flexbox-grid.js
+++ js/tests/casperjs/flexbox-grid.js
@@ -1,25 +1,53 @@
 var fs = require('fs');
+var SNAPSHOT_FOLDER = 'test-snapshots/';
+var viewports = [
+    {
+        'name': 'bootstrap-screen-xs',
+        'viewport': {width: 480, height: 1024}
+    },
+    {
+        'name': 'bootstrap-screen-sm',
+        'viewport': {width: 768, height: 1024}
+    },
+    {
+        'name': 'bootstrap-screen-md',
+        'viewport': {width: 992, height: 1024}
+    },
+    {
+        'name': 'bootstrap-screen-lg',
+        'viewport': {width: 1200, height: 1024}
+    }
+];
+
+function captureInVP(selector, image, viewport, snapshotFolder) {
+    var imagePath = snapshotFolder + viewport + '/' + image;
+    casper.captureSelector(imagePath, selector);
+}
+
+function compareFilesInFolder(path) {
+    var files = fs.list(path);
+    var referenceImageContent = fs.read(path + files[2]),
+        newImageContent = fs.read(path + files[3]);
+    casper.test.assert(newImageContent == referenceImageContent);
+
+}
 
 casper.start('http://0.0.0.0:9001/examples/grid/');
-
-casper.then(function(){
-    this.captureSelector('normal-grid-example-basics.png', '.test-three-equal-columns');
-    this.captureSelector('normal-grid-example-basics.png', '.test-three-unequal-columns');
+casper.each(viewports, function(casper, viewport) {
+    this.then(function() {
+        this.viewport(viewport.viewport.width, viewport.viewport.height);
+    });
+    casper.then(function(){
+        captureInVP('.test-three-equal-columns', 'normal-grid-example-basics.png', viewport.name, SNAPSHOT_FOLDER);
+    });
+    casper.thenOpen('http://0.0.0.0:9001/examples/flex-grid/');
+    casper.then(function(){
+        captureInVP('.test-three-equal-columns', 'flex-grid-example-basics.png', viewport.name, SNAPSHOT_FOLDER);
+    });
+    casper.then(function(){
+        compareFilesInFolder(SNAPSHOT_FOLDER + viewport.name + '/');
+    });
 });
-
-casper.thenOpen('http://0.0.0.0:9001/examples/flex-grid/');
-
-casper.then(function(){
-    this.captureSelector('flex-grid-example-basics.png', '.test-three-equal-columns');
-    this.captureSelector('flex-grid-example-basics.png', '.test-three-unequal-columns');
-});
-
-casper.then(function(){
-    var referenceImageContent = fs.read('./normal-grid-example-basics.png'),
-        newImageContent = fs.read('./flex-grid-example-basics.png');
-    this.test.assert(newImageContent == referenceImageContent);
-});
-
 
 casper.run(function(){
     this.echo('finished');
