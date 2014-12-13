f9000b14be0cd12a8cbcd79033201a4be777e51b
Vladimir Plokhov <vladimirplokhov@gmail.com>
2014-12-14 00:46:37 +0100

    Improve tests (casperjs)

--- js/tests/casperjs/flexbox-grid.js
+++ js/tests/casperjs/flexbox-grid.js
@@ -24,33 +24,59 @@ function captureInVP(selector, image, viewport, snapshotFolder) {
     casper.captureSelector(imagePath, selector);
 }
 
-function compareFilesInFolder(path) {
-    var files = fs.list(path);
-    var referenceImageContent = fs.read(path + files[2]),
-        newImageContent = fs.read(path + files[3]);
-    casper.test.assert(newImageContent == referenceImageContent);
-
+function stringStartsWith(substr) {
+    return function(element) {
+        return element.indexOf(substr) === 0;
+    }
 }
 
-casper.start('http://0.0.0.0:9001/examples/grid/');
+function compareFilesInFolder(path, refFilePrefix, newFilePrefix) {
+    var files = fs.list(path),
+        newFiles = files.filter(stringStartsWith(newFilePrefix)),
+        refFiles = files.filter(stringStartsWith(refFilePrefix));
+    for (var i = 0, len = refFiles.length; i < len; i++) {
+        var referenceImageContent = fs.read(path + refFiles[i]),
+            newImageContent = fs.read(path + newFiles[i]);
+        casper.test.assert(newImageContent == referenceImageContent);
+    }
+}
+
+casper.start();
 casper.each(viewports, function(casper, viewport) {
-    this.then(function() {
+    casper.thenOpen('http://0.0.0.0:9001/examples/grid/');
+    casper.then(function() {
         this.viewport(viewport.viewport.width, viewport.viewport.height);
     });
-    casper.then(function(){
-        captureInVP('.test-three-equal-columns', 'normal-grid-example-basics.png', viewport.name, SNAPSHOT_FOLDER);
+    casper.then(function() {
+        casper.test.comment('Capture mode: Normal Grid, Viewport: ' + viewport.name);
+        captureInVP('.test-three-equal-columns', 'normal-grid-three-equal-columns.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-three-unequal-columns', 'normal-grid-three-unequal-columns.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-two-columns', 'normal-grid-two-columns.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-two-columns-with-two-nested-columns', 'normal-grid-two-columns-with-two-nested-columns.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-mixed-mobile-desktop', 'normal-grid-mixed-mobile-desktop.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-mixed-mobile-tablet-desktop', 'normal-grid-mixed-mobile-tablet-desktop.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-column-clearing', 'normal-grid-column-clearing.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-offset-push-pull', 'normal-grid-offset-push-pull.png', viewport.name, SNAPSHOT_FOLDER);
     });
     casper.thenOpen('http://0.0.0.0:9001/examples/flex-grid/');
-    casper.then(function(){
-        captureInVP('.test-three-equal-columns', 'flex-grid-example-basics.png', viewport.name, SNAPSHOT_FOLDER);
+    casper.then(function() {
+        casper.test.comment('Capture mode: Flex Grid, Viewport: ' + viewport.name);
+        captureInVP('.test-three-equal-columns', 'flex-grid-three-equal-columns.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-three-unequal-columns', 'flex-grid-three-unequal-columns.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-two-columns', 'flex-grid-two-columns.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-two-columns-with-two-nested-columns', 'flex-grid-two-columns-with-two-nested-columns.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-mixed-mobile-desktop', 'flex-grid-mixed-mobile-desktop.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-mixed-mobile-tablet-desktop', 'flex-grid-mixed-mobile-tablet-desktop.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-column-clearing', 'flex-grid-column-clearing.png', viewport.name, SNAPSHOT_FOLDER);
+        captureInVP('.test-offset-push-pull', 'flex-grid-offset-push-pull.png', viewport.name, SNAPSHOT_FOLDER);
     });
-    casper.then(function(){
-        compareFilesInFolder(SNAPSHOT_FOLDER + viewport.name + '/');
+    casper.then(function() {
+        casper.test.comment('Image comparison... Viewport: ' + viewport.name);
+        compareFilesInFolder(SNAPSHOT_FOLDER + viewport.name + '/', 'flex', 'normal');
     });
 });
 
-casper.run(function(){
-    this.echo('finished');
+casper.run(function() {
     this.test.done();
     this.test.renderResults(true);
 });
